52de584feaa3a11373ac267dc5085b73bcd44107
Bryan McClain <bamcclain10@gmail.com>
2021-04-13 13:58:50 -0400

    Webpack now compiles much faster

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.ts',
   devtool: false /* 'inline-source-map', */,
+  mode: 'development',
   devServer: {
     contentBase: './build',
   },
