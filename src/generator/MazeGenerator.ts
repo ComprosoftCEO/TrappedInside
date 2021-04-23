c2c76a2fb8d3b49b025a596019f5029bf919e2a9
Bryan McClain <bamcclain10@gmail.com>
2021-04-22 22:12:07 -0400

    Maze generator can now convert boolean maze to tree structure

--- src/generator/MazeGenerator.ts
+++ src/generator/MazeGenerator.ts
@@ -1,5 +1,6 @@
 import { MazeObject } from 'areas/MazeObject';
 import { MazeWallsGenerator } from './MazeWallsGenerator';
+import { buildTreeNodes } from './TreeNode';
 
 /**
  * Procedural algorithm to generate random mazes
@@ -20,11 +21,11 @@ export class MazeGenerator {
     this.centerTemplate = centerTemplate;
   }
 
-  private get centerWidth(): number {
+  private get templateWidth(): number {
     return this.centerTemplate[0].length;
   }
 
-  private get centerHeight(): number {
+  private get templateHeight(): number {
     return this.centerTemplate.length;
   }
 
@@ -32,13 +33,75 @@ export class MazeGenerator {
    * Complicated method to actually generate the maze
    */
   public generateMaze(): MazeObject[][] {
-    const generator = new MazeWallsGenerator(this.width, this.height, this.centerWidth, this.centerHeight);
+    const generator = new MazeWallsGenerator(this.width, this.height, this.templateWidth, this.templateHeight);
     const walls = generator.generateRandomMaze();
 
     const objects: MazeObject[][] = walls.map((row) => row.map((col) => (col ? MazeObject.Wall : MazeObject.Empty)));
     this.fillTemplate(objects);
 
+    // Convert the maze walls into a tree
+    const [rootRow, rootCol] = this.getRootNode();
+    const nodes = buildTreeNodes(walls, rootRow, rootCol);
+
     return objects;
+  }
+
+  /**
+   * Find the root node from the template by searching for the big door.
+   * ONLY searches the perimeter to find it.
+   *
+   * If no big door is found, then it returns the center above the top door.
+   */
+  private getRootNode(): [number, number] {
+    const topRow = this.getTemplateTopRow();
+    const bottomRow = topRow + this.templateHeight - 1;
+    const leftCol = this.getTemplateLeftColumn();
+    const rightCol = leftCol + this.templateWidth - 1;
+
+    // Top row
+    for (let col = 0; col < this.templateWidth; col += 1) {
+      if (this.centerTemplate[0][col] === MazeObject.BigDoor) {
+        return [topRow - 1, leftCol + col];
+      }
+    }
+
+    // Bottom row
+    for (let col = 0; col < this.templateWidth; col += 1) {
+      if (this.centerTemplate[this.templateHeight - 1][col] === MazeObject.BigDoor) {
+        return [bottomRow + 1, leftCol + col];
+      }
+    }
+
+    // Left column
+    for (let row = 0; row < this.templateHeight; row += 1) {
+      if (this.centerTemplate[row][0] === MazeObject.BigDoor) {
+        return [topRow + row, leftCol - 1];
+      }
+    }
+
+    // Right column
+    for (let row = 0; row < this.templateHeight; row += 1) {
+      if (this.centerTemplate[row][this.templateWidth - 1] === MazeObject.BigDoor) {
+        return [topRow + row, rightCol + 1];
+      }
+    }
+
+    // By default, return the center above the top row
+    return [topRow - 1, leftCol + Math.floor(this.templateWidth / 2)];
+  }
+
+  /**
+   * Compute the absoute top row for the template
+   */
+  private getTemplateTopRow(): number {
+    return Math.floor((2 * this.height + 1 - this.templateHeight) / 2) + Number(!(this.templateHeight % 2));
+  }
+
+  /**
+   * Compute the absoute left column for the template
+   */
+  private getTemplateLeftColumn(): number {
+    return Math.floor((2 * this.width + 1 - this.templateWidth) / 2) + Number(!(this.templateWidth % 2));
   }
 
   /**
@@ -46,10 +109,10 @@ export class MazeGenerator {
    */
   private fillTemplate(objects: MazeObject[][]): void {
     // Shift everything down by one if the height is even, and add an extra row of walls
-    const topRow = Math.floor((2 * this.height + 1 - this.centerHeight) / 2) + Number(!(this.centerHeight % 2));
-    const bottomRow = topRow + this.centerHeight;
-    const leftCol = Math.floor((2 * this.width + 1 - this.centerWidth) / 2) + Number(!(this.centerWidth % 2));
-    const rightCol = leftCol + this.centerWidth;
+    const topRow = this.getTemplateTopRow();
+    const bottomRow = topRow + this.templateHeight;
+    const leftCol = this.getTemplateLeftColumn();
+    const rightCol = leftCol + this.templateWidth;
 
     for (let row = topRow; row < bottomRow; row += 1) {
       for (let col = leftCol; col < rightCol; col += 1) {
--- src/generator/TreeNode.ts
+++ src/generator/TreeNode.ts
@@ -1,10 +1,65 @@
+import { MazeObject } from 'areas/MazeObject';
+
 /**
  * Single tree node in the maze
  */
 export interface TreeNode {
   row: number;
   column: number;
+  object: MazeObject;
 
   parent: TreeNode | null;
   children: TreeNode[];
+}
+
+// Up, down, left, right deltas
+const ALL_DIRECTIONS: [number, number][] = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+
+/**
+ * Convert a boolean wall maze to a tree structure.
+ *
+ * This method assumes that the maze walls are perfectly connected, or else it will loop forever.
+ *
+ * @param maze The walls inside the maze
+ * @param rootRow Row for the root node
+ * @param rootCol Column for the root node
+ * @param lastPosition Parent location in the maze. Used for recursion, leave undefined if calling this in the root.
+ */
+export function buildTreeNodes(
+  maze: boolean[][],
+  rootRow: number,
+  rootCol: number,
+  lastPosition?: [number, number],
+): TreeNode {
+  const rootNode: TreeNode = {
+    row: rootRow,
+    column: rootCol,
+    object: MazeObject.Empty,
+    parent: null,
+    children: [],
+  };
+
+  // Recursively add all children to the tree using depth-first search
+  for (const [deltaRow, deltaCol] of ALL_DIRECTIONS) {
+    const newRow = rootRow + deltaRow;
+    const newCol = rootCol + deltaCol;
+
+    if (maze[newRow][newCol]) {
+      continue; /* There is a wall here */
+    }
+    if (typeof lastPosition !== 'undefined' && newRow === lastPosition[0] && newCol === lastPosition[1]) {
+      continue; /* We have already been here */
+    }
+
+    const child = buildTreeNodes(maze, newRow, newCol, [rootRow, rootCol]);
+    child.parent = rootNode;
+    rootNode.children.push(child);
+  }
+
+  return rootNode;
 }
