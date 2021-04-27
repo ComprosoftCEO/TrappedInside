9a96456ea1bd6bdbeb1661f8f82b85e7ecc015ad
Bryan McClain <bamcclain10@gmail.com>
2021-04-26 22:07:37 -0400

    Added a favicon to the game

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Trapped Inside',
       template: 'src/index.html',
+      favicon: 'assets/images/BlueKey.png',
     }),
   ],
   module: {
